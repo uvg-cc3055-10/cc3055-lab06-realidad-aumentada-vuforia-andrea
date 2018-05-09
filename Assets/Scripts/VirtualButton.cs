ddb76ccb76017b9c7d943278aae179a244845d00
DESKTOP-PO2B61U\cooli <arg17801@uvg.edu.gt>
2018-05-09 08:55:57 -0600

    Lab terminado

---
+++ Assets/Scripts/VirtualButton.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Andrea Arguello 17801  
+ * VirtualButton.cs
+ * 9/5/18
+ * Configuracion del boton virtual de vuforia
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia; 
+
+//Implementa la interfaz IVirtualButtonEventHandler
+public class VirtualButton : MonoBehaviour, IVirtualButtonEventHandler
+{
+    VirtualButtonBehaviour virtualBtn;
+    public Animator birdAnim;
+
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+        //Le da play al audio al presionar el boton
+        birdAnim.SetTrigger("sing");
+        //Comprobar si si esta reaccionando
+        Debug.Log("Boton");
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        //throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+        virtualBtn = GetComponent<VirtualButtonBehaviour>();
+        virtualBtn.RegisterEventHandler(this);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
